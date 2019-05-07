9bff6a221d21e65349a0faf1a66acb559108626c
Michael A. Chapman <chapmanm305@gmail.com>
2019-05-07 08:59:06 -0700

    Set up basic hello world route, set up index.ejs and base.css files

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
+let newSearch = (req, res) => {
+  res.render('pages/index');
+};
+
+let getHello = (req, res) => {
+  res.render('pages/index');
+};
+
+app.get('/', newSearch);
+app.get('/hello', getHello);
+
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
